acee4c276a2bf3c077b8635e6c4fd8c0009449b7
shaik-sadhiks007 <shaik.sadhiks007@gmail.com>
2025-06-05 21:05:21 +0530

    todo application

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,131 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Todo from './Todo.jsx'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // js --- to be implemented before return
+
+
+  // useState ---- function (hooks) which is used to update the html
+
+
+  // syntax ----
+
+  // instead of creating variables like
+
+  // let sadhik = 5;  --- creating
+
+  // sadhik = 7 --- updating
+
+
+
+
+  const [sadhik, setSadhik] = useState(20);
+
+  // let sadhik1 = 202
+
+  // sadhik1 = 1
+
+  /// [variablename, functionname] = useState(initialvalue)
+
+
+  const name = "sadhik";
+
+
+  let count = 0;
+
+
+  console.log(count, 'count')
+
+  function increment() {
+
+
+    // count++
+
+    count = count + 1
+
+    console.log(count, "count in fun")
+
+  }
+
+  function sadhikincrement() {
+
+
+    // count = count+ 1
+
+    setSadhik(sadhik + 1)
+
+  }
+
+
+  function sadhikdecrement() {
+
+
+    // count = count+ 1
+
+    setSadhik(sadhik - 1)
+
+  }
+
+  function sadhikreset() {
+
+    setSadhik(0)
+
+  }
+
 
   return (
-    <>
+
+    <div>
+
+      {/* html */}
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        username is {name}
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <h1>Count : {count}</h1>
+
+
+
+      {/* // don't give function call */}
+
+      {/* 1.method --- don't use the parenthesis */}
+
+      <button onClick={increment}>increment</button>
+
+
+      <h1>count of sadhik : {sadhik} </h1>
+
+      <button onClick={sadhikincrement}>increment</button>
+
+      <button onClick={sadhikdecrement}>decrement</button>
+
+      <button onClick={sadhikreset}>reset</button>
+
+
+
+
+
+      {/* // 2.method ---- callback function */}
+
+
+      {/* <button onClick={() => increment()}>increment</button> */}
+
+
+
+
+      {/* divtag.addeventlister('click', increment ) */}
+
+
+
+    </div>
+
   )
+
 }
 
-export default App
+export default App;
---
+++ src/Todo.jsx
@@ -0,0 +1,12 @@
+
+
+function Todo() {
+
+    return (
+
+        <h1>this is Todo app</h1>
+    )
+}
+
+export default Todo;
+
